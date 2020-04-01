72ad08f7da6592c023480cb2384b986f76808939
ojaswinjosan <ojaswin.josan@gmail.com>
2020-04-01 19:51:36 +1100

    win10.ps1: Add self-elevation

--- win10.ps1
+++ win10.ps1
@@ -1,3 +1,9 @@
+# Run as Administrator
+If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]'Administrator')) {
+    Start-Process powershell.exe -ArgumentList ("-NoProfile -ExecutionPolicy Bypass -File `"{0}`"" -f $PSCommandPath) -Verb RunAs
+    Exit
+}
+
 # Create a temp directory
 New-Item -Type directory -Force -Path C:\Users\$env:UserName\Downloads\temp | Out-Null
 Set-Location "C:\Users\$env:UserName\Downloads\temp"
