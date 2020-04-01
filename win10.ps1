08dd20024dba852db80d3ddd9191914aebc676f5
ojaswinjosan <ojaswin.josan@gmail.com>
2020-04-01 23:03:57 +1100

    win10.ps1: Add Visual C++ Redistributable 2005

--- win10.ps1
+++ win10.ps1
@@ -7,3 +7,14 @@ If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]:
 # Create a temp directory
 New-Item -Type directory -Force -Path C:\Users\$env:UserName\Downloads\temp | Out-Null
 Set-Location "C:\Users\$env:UserName\Downloads\temp"
+
+# Visual C++ Redistributables
+Write-Host "Downloading Visual C++ Redistributable 2005"
+
+(New-Object System.Net.WebClient).DownloadFile("https://download.microsoft.com/download/8/B/4/8B42259F-5D70-43F4-AC2E-4B208FD8D66A/vcredist_x86.EXE", "C:\Users\$env:UserName\Downloads\temp\vcredist_2005_x86.exe")
+$proc = Start-Process "vcredist_2005_x86.exe" -Wait -ArgumentList "/Q" -PassThru
+$proc.WaitForExit()
+
+(New-Object System.Net.WebClient).DownloadFile("https://download.microsoft.com/download/8/B/4/8B42259F-5D70-43F4-AC2E-4B208FD8D66A/vcredist_x64.EXE", "C:\Users\$env:UserName\Downloads\temp\vcredist_2005_x64.exe")
+$proc = Start-Process "vcredist_2005_x64.exe" -Wait -ArgumentList "/Q" -PassThru
+$proc.WaitForExit()
