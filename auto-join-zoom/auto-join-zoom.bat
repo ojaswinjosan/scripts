2aa108d5b54d68e43a93f0a1abfccea99744a6ef
ojaswinjosan <ojaswin.josan@gmail.com>
2020-04-25 07:34:23 +1000

    auto-join-zoom.bat: Disable the option to skip timeouts

--- auto-join-zoom/auto-join-zoom.bat
+++ auto-join-zoom/auto-join-zoom.bat
@@ -1,6 +1,6 @@
-TIMEOUT /t 2
+TIMEOUT /t 2 /nobreak
 start BROWSER /new-window URL
-TIMEOUT /t 3
+TIMEOUT /t 3 /nobreak
 echo Starting Zoom
-TIMEOUT /t 2
+TIMEOUT /t 2 /nobreak
 start %~dp0/start-zoom.ahk
