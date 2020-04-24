86008b856765dafb7bfd00ea4849645368476edf
ojaswinjosan <ojaswin.josan@gmail.com>
2020-04-25 08:40:47 +1000

    auto-join-zoom.bat: Adjust timeouts

--- auto-join-zoom/auto-join-zoom.bat
+++ auto-join-zoom/auto-join-zoom.bat
@@ -6,7 +6,7 @@ SET URL=
 TIMEOUT /t 2 /nobreak
 ECHO Starting %BROWSER%
 IF "%BROWSER%"=="edge" (START microsoft-edge:%URL%) ELSE (START %BROWSER% /new-window %URL%)
-TIMEOUT /t 3 /nobreak
-echo Starting Zoom
 TIMEOUT /t 2 /nobreak
+echo Starting Zoom
+TIMEOUT /t 8 /nobreak
 start %~dp0/start-zoom.ahk
