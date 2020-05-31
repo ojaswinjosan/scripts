15692d8008062a7b1982ef704ba7b0383720b11d
ojaswinjosan <ojaswin.josan@gmail.com>
2020-06-01 05:46:38 +1000

    auto-join-zoom.bat: Turn off echo for all commands

--- auto-join-zoom/auto-join-zoom.bat
+++ auto-join-zoom/auto-join-zoom.bat
@@ -1,13 +1,14 @@
+@echo off
 REM Browsers Tested: chrome, firefox, opera, edge
 REM Make sure there's no space after the = sign
 SET BROWSER=edge
 SET URL=
 SET AHK-LOCATION="C:\Program Files\AutoHotkey\AutoHotkey.exe"
 
-TIMEOUT /t 2 /nobreak
+TIMEOUT /t 2 /nobreak > NUL
 ECHO Starting %BROWSER%
 IF "%BROWSER%"=="edge" (START microsoft-edge:%URL%) ELSE (START %BROWSER% /new-window %URL%)
-TIMEOUT /t 2 /nobreak
+TIMEOUT /t 2 /nobreak > NUL
 echo Starting Zoom
-TIMEOUT /t 8 /nobreak
+TIMEOUT /t 8 /nobreak > NUL
 %AHK-LOCATION% "%~dp0\start-zoom.ahk"
